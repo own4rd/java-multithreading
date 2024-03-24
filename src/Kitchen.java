b4f2ff3ea36d832ae6d6584d87b947cf1a7375e5
Wagner <wagnerpalmeira.ti@gmail.com>
2024-03-24 12:47:11 -0300

    Example deadlock

---
+++ src/Kitchen.java
@@ -0,0 +1,28 @@
+public class Kitchen {
+    public static Object spoon = new Object();
+    public static Object bowl = new Object();
+    public static void main(String[] args) {
+        Thread cook1 = new Thread(() -> {
+            synchronized (spoon) {
+                System.out.println("Cook1: Holding the spoon");
+                System.out.println("Cook1: Waiting for the bowl");
+                synchronized (bowl) {
+                    System.out.println("Cock1: Holding the spoon and the bowl.");
+                }
+            }
+        });
+//        TO AVOID, PUT SPOON FIRST HERE TOO
+        Thread cook2 = new Thread(() -> {
+            synchronized (bowl) {
+                System.out.println("Cook2: Holding the bowl");
+                System.out.println("Cook2: Waiting for the spoon");
+                synchronized (spoon) {
+                    System.out.println("Cook2: Holding the spoon and the bowl.");
+                }
+            }
+        });
+
+        cook1.start();
+        cook2.start();
+    }
+}
